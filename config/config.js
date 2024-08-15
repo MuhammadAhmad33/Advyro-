35ef1891a8a935f42d8d378558be1a1099950e35
Createex Mac <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-08-15 06:58:50 +0500

    azure blob fix

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ module.exports = {
     mongoURI: 'mongodb+srv://Sleek:1Hmi6EHEptDrvFdU@cluster0.v6ci4ot.mongodb.net/',
     JWT_SECRET:'Advyro',
     resendApiKey:'re_KXbbyurt_6evMKBFkGNgMaWwCYpQk2qnN',
-    STRIPE_SECRET_KEY:'sk_test_51PWFAtRsuZrhcR6RkARSyeTININjrF9jCGyX578O6uoZJBjtuTYwcIfjGarejovYy4O1ONuLhiiZksJpcRU5BUzE0063W9ijCB'
-
+    STRIPE_SECRET_KEY:'sk_test_51PWFAtRsuZrhcR6RkARSyeTININjrF9jCGyX578O6uoZJBjtuTYwcIfjGarejovYy4O1ONuLhiiZksJpcRU5BUzE0063W9ijCB',
+    AZURE_STORAGE_CONNECTION_STRING:'DefaultEndpointsProtocol=https;AccountName=advyrostorage;AccountKey=kTxhsbD7Nge/cLN3s8aZDJPpq3bw/g95XmrgvGaJ8jAoHO6YdeONCiEmoEChbEi0lBXs09wAlW6g+AStrifBWA==;EndpointSuffix=core.windows.net',
+    AZURE_CONTAINER_NAME:'advyrocontainer'
 };
